43d033ff987d8c38c2da74b314787a43990a770a
hassansin <mhkr63@gmail.com>
2016-08-01 09:15:09 +0600

    Init

---
+++ test.sh
@@ -0,0 +1,91 @@
+#! /bin/sh
+
+setUp() {
+    originalPath=$PATH
+    PATH=$PWD:$PATH
+	touch GIT_BRANCH
+    touch COMMIT_MSG
+}
+
+tearDown() {
+    PATH=$originalPath
+    rm -f GIT_BRANCH COMMIT_MSG
+}
+
+
+testNoIssueInBranch()
+{
+    echo 'commit message' > COMMIT_MSG
+    echo 'master' > GIT_BRANCH
+    ./prepare-commit-msg ./COMMIT_MSG "message"
+    assertEquals "$(cat ./COMMIT_MSG)" "commit message"
+}
+
+testCapitalCaseBranch()
+{
+    echo 'commit message' > COMMIT_MSG
+    echo 'CW-255' > GIT_BRANCH
+    ./prepare-commit-msg ./COMMIT_MSG "message"
+    assertEquals "$(cat ./COMMIT_MSG)" "#CW-255 commit message"
+}
+
+testLowerCaseBranch()
+{
+    echo 'commit message' > COMMIT_MSG
+    echo 'cw-255' > GIT_BRANCH
+    ./prepare-commit-msg ./COMMIT_MSG "message"
+    assertEquals "$(cat ./COMMIT_MSG)" "#CW-255 commit message"
+}
+
+
+testExtraTextAfterBranch()
+{
+    echo 'commit message' > COMMIT_MSG
+    echo 'CW-255-some-text' > GIT_BRANCH
+    ./prepare-commit-msg ./COMMIT_MSG "message"
+    assertEquals "$(cat ./COMMIT_MSG)" "#CW-255 commit message"
+}
+
+testExtraTextBeforeBranch()
+{
+    echo 'commit message' > COMMIT_MSG
+    echo 'some-branch-cww-100' > GIT_BRANCH
+    ./prepare-commit-msg ./COMMIT_MSG "message"
+    assertEquals "$(cat ./COMMIT_MSG)" "#CWW-100 commit message"
+}
+
+
+testDifferentIssueInCommitAndBranch_1()
+{
+    echo 'some commit message BW-100' > COMMIT_MSG
+    echo 'CW-100' > GIT_BRANCH
+    ./prepare-commit-msg ./COMMIT_MSG "message"
+    assertEquals "$(cat ./COMMIT_MSG)" "#CW-100 some commit message BW-100"
+}
+
+testDifferentIssueInCommitAndBranch_2()
+{
+    echo 'some commit message CWW-100 and more' > COMMIT_MSG
+    echo 'CW-100' > GIT_BRANCH
+    ./prepare-commit-msg ./COMMIT_MSG "message"
+    assertEquals "$(cat ./COMMIT_MSG)" "#CW-100 some commit message CWW-100 and more"
+}
+
+testSameIssueInCommitAndBranch_1()
+{
+    echo 'some commit message CW-100' > COMMIT_MSG
+    echo 'CW-100' > GIT_BRANCH
+    ./prepare-commit-msg ./COMMIT_MSG "message"
+    assertEquals "$(cat ./COMMIT_MSG)" "some commit message CW-100"
+}
+
+testMultipleIssuesInCommit()
+{
+    echo 'some commit message pw-1111,cw-100 and bw-200' > COMMIT_MSG
+    echo 'CW-100-some-text' > GIT_BRANCH
+    ./prepare-commit-msg ./COMMIT_MSG "message"
+    assertEquals "$(cat ./COMMIT_MSG)" "some commit message pw-1111,cw-100 and bw-200"
+}
+
+# load shunit2
+. ./shunit2
